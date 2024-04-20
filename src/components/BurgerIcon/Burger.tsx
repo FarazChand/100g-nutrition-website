89824dc58a3ba55913e7296d1b2487f1589bc423
Faraz Chand <faraz.chand@hotmail.com>
2024-04-19 22:03:34 -0400

    general update

--- src/components/BurgerIcon/Burger.tsx
+++ src/components/BurgerIcon/Burger.tsx
@@ -1,44 +1,35 @@
 import { useState } from "react";
 
 const Burger = (props) => {
-  const [line1Rotate, setLine1Roatate] = useState("rotate-0");
-  const [line2Rotate, setLine2Roatate] = useState("opacity-1");
-  const [line3Rotate, setLine3Roatate] = useState("rotate-0");
+  const { isOpen, navHandler } = props;
 
-  const burgerMenuClickedHandler = () => {
-    if (props.isOpen === true) {
-      setLine1Roatate(
-        "rotate-0 translate-x-[0px] translate-y-[0px] rounded-sm",
-      );
-      setLine2Roatate("opacity-1 rounded-sm");
-      setLine3Roatate(
-        "rotate-0 translate-x-[0px] translate-y-[0px] rounded-sm",
-      );
-    } else {
-      setLine1Roatate(
-        "-rotate-45 translate-x-[0px] translate-y-[6px] rounded-sm",
-      );
-      setLine2Roatate("opacity-0");
-      setLine3Roatate(
-        "rotate-45 translate-x-[0px] translate-y-[-7.75px] rounded-sm",
-      );
-    }
-    props.navHandler();
+  const burgerState = {
+    line1Rotate: isOpen
+      ? "rotate-0 translate-x-[0px] translate-y-[0px] rounded-sm"
+      : "-rotate-45 translate-x-[0px] translate-y-[6px] rounded-sm",
+    line2Rotate: isOpen ? "opacity-1 rounded-sm" : "opacity-0",
+    line3Rotate: isOpen
+      ? "rotate-0 translate-x-[0px] translate-y-[0px] rounded-sm"
+      : "rotate-45 translate-x-[0px] translate-y-[-7.75px] rounded-sm",
+  };
+
+  const toggleBurgerMenuHandler = () => {
+    navHandler();
   };
 
   return (
     <div
-      onClick={burgerMenuClickedHandler}
-      className="sm-max:block sm-max:cursor-pointer"
+      onClick={toggleBurgerMenuHandler}
+      className="sm-max:block sm-max:cursor-pointer hidden"
     >
       <div
-        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${line1Rotate}`}
+        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${burgerState.line1Rotate}`}
       ></div>
       <div
-        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${line2Rotate}`}
+        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${burgerState.line2Rotate}`}
       ></div>
       <div
-        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${line3Rotate}`}
+        className={`m-1 h-[3px] w-6 bg-blue-500 transition duration-500 ${burgerState.line3Rotate}`}
       ></div>
     </div>
   );
