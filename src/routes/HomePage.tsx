c05deb933209cbfdb4d5ad95aa753b512d293d4e
Faraz Chand <faraz.chand@hotmail.com>
2024-05-24 22:33:13 -0400

    general update

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -7,6 +7,9 @@ export default function HomePage() {
       <p>Intro Text</p>
       <p>Intro Text</p>
       <p>Intro Text</p>
+      <p>Intro Text</p>
+      <p>Intro Text</p>
+      <p>Intro Text</p>
     </main>
   );
 }
