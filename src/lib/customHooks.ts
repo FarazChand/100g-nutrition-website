543d5be9207b52e5b42b31b6b1b9d490f377f903
Faraz Chand <faraz.chand@hotmail.com>
2024-07-31 16:58:02 -0400

    general update

--- src/lib/customHooks.ts
+++ src/lib/customHooks.ts
@@ -8,7 +8,7 @@ const apiURL = process.env.REACT_APP_API_BASE_URL
   ? process.env.REACT_APP_API_BASE_URL
   : "http://localhost:5000";
 
-console.log(process.env.REACT_APP_API_BASE_URL);
+console.log("apiUrl:", process.env.REACT_APP_API_BASE_URL);
 
 // ----------------------------------------------
 
